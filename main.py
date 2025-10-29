387ed9255b5dcc5b170030c1c5cb0028ed390a48
Camilo Martinez <111545537+camartinezm92@users.noreply.github.com>
2025-10-29 13:36:50 -0500

    Update main.py
    Correction CORS

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:4200",  # para pruebas locales
-        "https://frontendbitandman.onrender.com"
-        "https://oxp-bitacora-man.onrender.com"  # si luego subes el
+        "https://frontendbitandman.onrender.com",
+        "https://oxp-bitacora-man.onrender.com",  # si luego subes el
         # front a Render
     ],
     allow_credentials=True,
